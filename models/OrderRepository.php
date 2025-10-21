98789eab05d982aee1ce27f70734ccac189af107
Guille2701 <tortillaconmayonesa27@gmail.com>
2025-10-21 11:12:58 +0200

    Implement order retrieval functionality and add order history view template

--- models/OrderRepository.php
+++ models/OrderRepository.php
@@ -1,9 +1,25 @@
 <?php
 
 class OrderRepository {
-    public function getOrderDetailsByOrderId($order_id) {
+    
+    public function getOrdersByUserId($user_id) {
         $db = Connection::connect();
+        $q = "SELECT * FROM orders WHERE user_id = ?";
+        $result = $db->query($q);
+        $orders = [];
+        while ($row = mysqli_fetch_assoc($result)) {
+            $orders[] = new Order(
+                $row['id'],
+                $row['user_id'],
+                $row['total'],
+                $row['status'],
+                $row['order_date']
+            );
+        }
+        return $orders;
+        
     }
+
 }
 
 ?>
